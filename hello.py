cc959dfe5c12528e5a20e261a2efb265d2160167
SO TAKAHASHI <sotakahashi@RCMRF.local>
2018-02-07 00:06:36 +0900

    pep8

--- hello.py
+++ hello.py
@@ -4,11 +4,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
+
 if __name__ == '__main__':
-    host = os.getenv('IP','0.0.0.0')
+    host = os.getenv('IP', '0.0.0.0')
     port = int(os.getenv('PORT', 5000))
     app.run(host=host, port=port)
